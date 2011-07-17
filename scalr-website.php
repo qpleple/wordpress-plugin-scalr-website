b32b25a03594d3f3cc94f610d228df929216e6cd
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-16 18:42:16 -0700

    added post request with user login and password

--- scalr-website.php
+++ scalr-website.php
@@ -3,7 +3,27 @@
 Plugin Name: Scalr Website
 Plugin URI: http://scalr.net
 Description: This is where the logic of the Scalr public website lives.
-Version: 1.0
+Version: 1.1
 Author: Quentin Pleplé
 License: Apache
 */
+
+require_once dirname(__FILE__) . "/scalr_http_utils.php"
+
+function scalr_login_page() {
+    if (!(isset($_POST['email']) && isset($_POST['password']))) {
+        return;
+    }
+    
+    //// Test email valid
+    //if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    //    return;
+    //}
+    
+    var_dump(http_post("https://my.scalr.net/guest/xLogin/", array(
+        'scalrLogin' => $_POST['email'],
+        'scalrPass' => $_POST['password'],
+    )));
+}
+add_shortcode('scalr_login_page', 'scalr_login_page');
+
