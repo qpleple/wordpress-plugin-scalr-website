3ce91d7c9c0eafb9af217945179fd1342377a33f
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-16 18:54:49 -0700

    added response decoding

--- scalr_website.php
+++ scalr_website.php
@@ -8,8 +8,9 @@ Author: Quentin Pleplé
 License: Apache
 */
 
-require_once dirname(__FILE__) . "/scalr_http_utils.php"
+require_once dirname(__FILE__) . "/scalr_http_utils.php";
 
+// TODO: error handling, no more blank return
 function scalr_login_page() {
     if (!(isset($_POST['email']) && isset($_POST['password']))) {
         return;
@@ -20,9 +21,23 @@ function scalr_login_page() {
     //    return;
     //}
     
-    var_dump(http_post("https://my.scalr.net/guest/xLogin/", array(
+    $http_response = http_post("https://my.scalr.net/guest/xLogin/", array(
         'scalrLogin' => $_POST['email'],
         'scalrPass' => $_POST['password'],
-    )));
+    ));
+    
+    if (!is_array($http_response) || !array_key_exists("code", $http_response) || $http_response['code'] != "200") {
+        return;
+    }
+    
+    $response = @json_decode($http_response['body']);
+    if (empty($response) || !property_exists($response, "success")) {
+        return;
+    }
+    
+    if (!$response->success) {
+        echo $response->errorMessage;
+        return;
+    }
 }
 add_shortcode('scalr_login_page', 'scalr_login_page');
