03a7a9095e10b4f24e79a1dc1873c5934556de99
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-16 17:51:55 -0700

    added plugin main file

---
+++ scalr-website.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Scalr Website
+Plugin URI: http://scalr.net
+Description: This is where the logic of the Scalr public website lives.
+Version: 1.0
+Author: Quentin Pleplé
+License: Apache
+*/
